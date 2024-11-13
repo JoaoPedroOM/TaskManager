ea9abe9990047292f60ea77c0acad203026fbaff
João <joaopedromari28@gmail.com>
2024-11-13 14:57:51 -0300

    feat: Implementado efeito de cor dinâmica no título da home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,12 @@
+"use client";
 import React, { useState } from "react";
 import Header from "../components/global/Header";
 import Footer from "../components/global/Footer";
 import { Link } from "react-router-dom";
 import Skeleton from "@mui/material/Skeleton";
 import Box from "@mui/material/Box";
+import { motion } from "framer-motion";
+import { Highlight } from "../components/ui/hero-highlight";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -17,18 +20,30 @@ const Home = () => {
       <Header />
       <main className="lg:mb-[120px] lg:my-[120px] my-14">
         <div className="flex flex-col justify-center items-center gap-8">
-          <h1 className="font-main text-center lg:w-1/2 w-full lg:text-5xl lg:px-0 px-2 text-3xl font-bold text-[#d4d5d6]">
-            Melhoria na Produtividade e Gestão de Tarefas
-          </h1>
-          <p className="font-titles text-center font-light lg:text-xl text-base lg:px-0 px-2 text-[#a9abad]">
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: [20, -5, 0] }}
+            transition={{ duration: 0.5, ease: [0.4, 0.0, 0.2, 1] }}
+            className="font-main lg:leading-[1.2] text-center lg:w-1/2 w-full lg:text-5xl lg:px-0 px-2 text-3xl font-bold text-[#d4d5d6]"
+          >
+           <Highlight>Melhoria na Produtividade</Highlight> e Gestão de Tarefas
+          </motion.h1>
+          
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: [20, -5, 0] }}
+            transition={{ duration: 0.5, ease: [0.4, 0.0, 0.2, 1] }}
+            className="font-titles text-center font-light lg:text-xl text-base lg:px-0 px-2 text-[#a9abad]"
+          >
             Aproveite uma Gestão de Tarefas Sem Complicação com o Task Manager:
             Simplicidade e Eficiência!
-          </p>
+          </motion.p>
 
-          <Link to="/logar" className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20">
-            <span className="text-lg">
-              Começar agora
-            </span>
+          <Link
+            to="/logar"
+            className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20"
+          >
+            <span className="text-lg">Começar agora</span>
             <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
               <div className="relative h-full w-10 bg-white/20"></div>
             </div>
